d1917813b251ef39f94290053f8a1cd39f344c60
Sonu Pal <sonupal0840@example.com>
2025-03-08 10:41:41 +0530

    Aasets move to public from src

--- src/Pages/Certificates.jsx
+++ src/Pages/Certificates.jsx
@@ -6,18 +6,18 @@ const Certificates = () => {
     <section className={style.container}>
         <h2 className={style.title}>Certificates</h2>
         <div>
-            <img src="./src/Assets/Python basic.png" alt="" className={style.Certificates}/>
-            <img src="./src/Assets/python Codechef.png" alt="" className={style.Certificates}/>
-            <img src="./src/Assets/sql1.png" alt="" className={style.Certificates}/>
-            <img src="./src/Assets/Sql2.png" alt="" className={style.Certificates}/>
-            <img src="./src/Assets/Html.png" alt="" className={style.Certificates}/>
-            <img src="./src/Assets/C++.png" alt="" className={style.Certificates}/>
-            <img src="./src/Assets/Robonatus.png" alt="" className={style.Certificates}/>
-            <img src="./src/Assets/Ain.png" alt="" className={style.Certificates}/>
-            <img src="./src/Assets/Manager.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/Python basic.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/python Codechef.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/sql1.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/Sql2.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/Html.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/C++.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/Robonatus.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/Ain.png" alt="" className={style.Certificates}/>
+            <img src="./Assets/Manager.png" alt="" className={style.Certificates}/>
         </div>
         <button className={style.button}>
-        <a href="./src/Assets/Certificates.pdf" target="_blank">Download PDF</a>
+        <a href="./Assets/Certificates.pdf" target="_blank">Download PDF</a>
 
         </button>
     </section>
--- src/Pages/Tools.jsx
+++ src/Pages/Tools.jsx
@@ -9,18 +9,18 @@ const Tools = () => {
       <h2 className={style.title}>Tools</h2>
       <div>
         
-          <img src="./src/Assets/python.svg" alt=""  className={style.Icon}/>
-          <img src="./src/Assets/react.svg" alt="" className={style.Icon} />
-          <img src="./src/Assets/sql.svg" alt="" className={style.Icon} />
-          <img src="./src/Assets/django.svg" alt=""  className={style.Icon}/>
-          <img src="./src/Assets/html.svg" alt=""  className={style.Icon}/>
-          <img src="./src/Assets/css-alt.svg" alt=""  className={style.Icon}/>
-          <img src="./src/Assets/javascript.svg" alt="" className={style.Icon}/>
-          <img src="./src/Assets/canva.svg" alt="" className={style.Icon}/>
-          <img src="./src/Assets/figma.svg" alt="" className={style.Icon}/>
-          <img src="./src/Assets/git.svg" alt="" className={style.Icon}/>
-          <img src="./src/Assets/chart.js_logo.svg" alt="" className={style.Icon}/>
-          <img src="./src/Assets/word.png" alt="" className={style.Icon}/>
+          <img src="./Assets/python.svg" alt=""  className={style.Icon}/>
+          <img src="./Assets/react.svg" alt="" className={style.Icon} />
+          <img src="./Assets/sql.svg" alt="" className={style.Icon} />
+          <img src="./Assets/django.svg" alt=""  className={style.Icon}/>
+          <img src="./Assets/html.svg" alt=""  className={style.Icon}/>
+          <img src="./Assets/css-alt.svg" alt=""  className={style.Icon}/>
+          <img src="./Assets/javascript.svg" alt="" className={style.Icon}/>
+          <img src="./Assets/canva.svg" alt="" className={style.Icon}/>
+          <img src="./Assets/figma.svg" alt="" className={style.Icon}/>
+          <img src="./Assets/git.svg" alt="" className={style.Icon}/>
+          <img src="./Assets/chart.js_logo.svg" alt="" className={style.Icon}/>
+          <img src="./Assets/word.png" alt="" className={style.Icon}/>
        
       </div>
         
