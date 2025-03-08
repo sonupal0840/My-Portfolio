f82d87622ac2a8079b053ffb28fb1b9e9806b29f
Sonu Pal <sonupal0840@example.com>
2025-03-08 10:51:37 +0530

    Fixed image loading issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/My-Protfolio/", // 👈 अपने GitHub repo के नाम से बदलें
+  base: "/My-Protfolio/", // 👈 यहाँ अपना GitHub Repo Name डालें
 });
+
 
