439a9ae14d079020bf76a02ae8bf790e65095bad
Sonu Pal <sonupal0840@example.com>
2025-03-08 10:46:03 +0530

    Fixed image import issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from "./home.module.css"
-import myImg from "../Assets/myImg.jpg"
+// import myImg from "../Assets/myImg.jpg"
 
 const Home = () => {
     return (
         <div>
             <div className={style.container}>
-                <img src={myImg} alt="" />
+                {/* <img src={myImg} alt="" /> */}
+                <img src="/Assets/myImg.jpg" alt="Profile" />
                 <div className={style.profile}>
                     <p> ||Python Developer||</p>
                     <p>||Frontend Developer||</p>
