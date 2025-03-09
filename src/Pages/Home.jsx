0bbab57b405039520a725bbf4ea39f59ea6449c5
Sonu Pal <sonupal0840@example.com>
2025-03-09 19:48:03 +0530

    set env of myImg

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import style from "./home.module.css"
-// import myImg from "../../public/assets/myImg.jpg"
 
 const Home = () => {
     return (
         <div>
             <div className={style.container}>
-                {/* <img src={myImg} alt="" /> */}
-                {/* <img src="/assets/myImg.jpg" alt="Profile" /> */}
-                <img src="/assets/myImg.jpg" alt="Profile" />
+                <img src={`${process.env.PUBLIC_URL}/assets/myImg.jpg`} alt="Profile" />
                 <div className={style.profile}>
                     <p> ||Python Developer||</p>
                     <p>||Frontend Developer||</p>
