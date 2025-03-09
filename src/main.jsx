1892723fcf166b837ba772afa6560bc517134258
Sonu Pal <sonupal0840@example.com>
2025-03-09 15:18:45 +0530

    update HashRouter and deploy path

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./global.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/My-Portfolio"
+        <HashRouter basename="/My-Portfolio"
         future={{ v7_startTransition: true, v7_relativeSplatPath: true }}> 
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
